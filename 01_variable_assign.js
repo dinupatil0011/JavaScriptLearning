ba50fbee8809353de7278607a753cdd5cbdec696
dinupatil0011 <dineshpatil8484@gmail.com>
2022-12-08 13:14:51 +0530

    Create 01_variable_assignment.
    Create 01_variable_assignment.

---
+++ 01_variable_assign.js
@@ -0,0 +1,29 @@
+var myName;
+console.log(myName);
+
+myFullName = "Dinesh Nandaram Patil";
+console.log(myFullName);
+
+var myLove = "I Love only JS";
+console.log(myLove);
+
+var firstName = "Dinesh";
+console.log(firstName);
+
+var lastName = "Patil";
+console.log(lastName);
+
+var myAge = "24";
+console.log(myAge);
+
+var myAddress = "Jalgaon";
+console.log(myAddress);
+
+var pin_code = "425001";
+console.log(pin_code);
+
+var gender = "Male";
+console.log(gender);
+
+var is_married = "No";
+console.log(is_married);
