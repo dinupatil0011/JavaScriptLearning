f45e68c30fec4e5b601edeca5326f30200d61717
dinupatil0011 <dineshpatil8484@gmail.com>
2022-12-12 20:31:06 +0530

    Create 01_typeOf
    Create 01_typeOf

---
+++ 01_typeOf.js
@@ -0,0 +1,25 @@
+var bankName = "SBI Bank";
+console.log(bankName);
+console.log(typeof bankName);
+var line  = "-----------------------------------------";
+console.log(line);
+var marks = "90%"
+console.log(marks);
+console.log(typeof marks);
+var line  = "-----------------------------------------";
+console.log(line);
+var isWorking = "false"
+console.log(isWorking);
+console.log(typeof false);
+var line  = "-----------------------------------------";
+console.log(line);
+var totalCount = "Hundred and Seven"
+console.log(totalCount);
+console.log(typeof totalCount);
+var line  = "-----------------------------------------";
+console.log(line);
+var totalCount ="107"
+console.log(totalCount);
+console.log(typeof 107);
+var line  = "-----------------------------------------";
+console.log(line);
