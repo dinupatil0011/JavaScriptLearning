1ea10e7d5f1154eab6857b023e06ed79182dbf03
dinupatil0011 <dineshpatil8484@gmail.com>
2023-01-07 15:10:37 +0530

    Update string code.
    Update string code.

--- 03_string.js
+++ 03_string.js
@@ -6,7 +6,6 @@ console.log("Total character in the string is: ", firstName.length);
 console.log("Char at index 3: ", firstName.charAt(3));
 
 var lengthTotal = firstName.length;
-
 console.log("Char at last index : ", firstName.charAt(lengthTotal - 1));
 
 console.log("======== concat() ==========");
