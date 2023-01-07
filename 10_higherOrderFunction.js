c7f5d4e24cdc9622cf7ec60bbfa43f823f46cb9a
dinupatil0011 <dineshpatil8484@gmail.com>
2023-01-07 15:14:34 +0530

    Create a new code.
    Create a new code.

---
+++ 10_higherOrderFunction.js
@@ -0,0 +1,13 @@
+// Higher Order Function - 
+function anotherFunction(){
+    console.log("I am inside another function....");
+}
+function show(myNumber){ // myNumber is an argument
+    myNumber();
+    let innerFunction = function(){
+        console.log("Inner function.....");
+     }
+     return innerFunction;
+}
+ let inner = show(anotherFunction);
+ inner();
